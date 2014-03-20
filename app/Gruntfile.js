9704189a8853d4ca7978ea31d5a98636b220c88b
christian marienfeld <post@chrisland.de>
2014-03-20 17:16:47 +0100

    add win

--- app/Gruntfile.js
+++ app/Gruntfile.js
@@ -63,7 +63,7 @@ module.exports = function(grunt) {
 			app_version: '0.5',
 			build_dir: './release', // Where the build version of my node-webkit app is saved
 			mac: true, // We want to build it for mac
-			win: false, // We want to build it for win
+			win: true, // We want to build it for win
 			linux32: false, // We don't need linux32
 			linux64: false, // We don't need linux64
 			mac_icns: 'jsonbuilder.icns',
